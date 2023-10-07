40ef89600655190429973b52d6e453185466050e
joeyskeys <joey.skeys@gmail.com>
2023-10-07 16:56:42 +0800

    add rcp&rsqrt impl for sse42

--- include/yavl/vec_sse42.h
+++ include/yavl/vec_sse42.h
@@ -5,6 +5,96 @@
 namespace yavl
 {
 
+static inline __m128 rcp_sse42_impl(const __m128 m) {
+    // Copied from enoki with some extra comments
+#if defined(YAVL_X86_AVX512ER)
+    // rel err < 2^28, use as is
+    return _mm512_castps512_ps128(
+        _mm512_rcp28_ps(_mm512_castps128_ps512(m));
+    );
+    r = _mm_rcp14_ps(m);
+#else
+    __m128 r;
+#if defined(YAVL_X86_AVX512VL)
+    r = _mm_rcp14_ps(m);    // rel error < 2^-14
+#else
+    // Unluckily the only available option on my machine
+    // is the worst one...
+    r = _mm_rcp_ps(m);      // rel error < 1.5*2^-12
+#endif
+
+    // Refine with one Newton-Raphson iteration
+    // Function for the iteration:
+    //                  1/x - a
+    // X(n+1) = X(n) - --------- = X(n) + X(n) - aX(n)^2
+    //                   -1/x^2
+    __m128 t0 = _mm_add_ps(r, r),
+           t1 = _mm_mul_ps(r, m);
+
+#ifndef YAVL_X86_AVX512VL
+    __m128 ro = r;
+    (void) ro;
+#endif
+
+#if defined(YAVL_X86_FMA)
+    r = _mm_fnmadd_ps(t1, r, t0);
+#else
+    r = _mm_sub_ps(t0, _mm_mul_ps(r, t1));
+#endif
+
+#if defined(YAVL_X86_AVX512VL)
+    // This intrinsic is marvellous!
+    // The mask 0x00887A622 carefully setup all the response
+    // for the possible numeric situation.
+    // Checkout https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#ig_expand=15,24,2680,2681,509,509,3046,3046&text=fixupimm
+    return _mm_fixupimm_ps(r, m, _mm_set1_epi32(0x0087A622), 0);
+#else
+    // Use the origianl value if t1 contains NaN
+    return _mm_blendv_ps(r, ro, t1);
+#endif
+#endif
+}
+
+static inline __m128 rsqrt_sse42_impl(const __m128 m) {
+    // Copied from enoki with extra comments
+#if defined(YAVL_X86_AVX512ER)
+    return _mm512_castps512_ps128(
+        // rel err < 2^28, use as is
+        _mm512_rsqrt28_ps(_mm512_castps128_ps512(m));
+    )
+#else
+    __m128 r;
+#if defined(YAVL_X86_AVX512VL)
+    r = _mm_rsqrt14_ps(m);  // rel err < 2^-14
+#else
+    r = _mm_rsqrt_ps(m);    // rel err < 1.5*2^-12
+#endif
+
+    // Refine with one Newton-Raphson iteration
+    const __m128 c0 = _mm_set1_ps(.5f),
+                 c1 = _mm_set1_ps(3.f);
+
+    __m128 t0 = _mm_mul_ps(r, c0),
+           t1 = _mm_mul_ps(r, m);
+#ifndef YAVL_x86_AVX512VL
+           ro = r;
+    (void) ro;
+#endif
+
+#if defined(YAVL_X86_FMA)
+    r = _mm_mul_ps(_mm_fnmadd_ps(t1, r, c1), t0);
+#else
+    r = _mm_mul_ps(_mm_sub_ps(c1, _mm_mul_ps(t1, r)), t0);
+#endif
+
+#if defined(YAVL_X86_AVX512VL)
+    return _mm_fixupimm_ps(r, m, _mm_set1_epi32(0x383A622), 0);
+#else
+    return _mm_blendv_ps(r, ro, t1);
+#endif
+#endif
+}
+
 template <>
 struct alignas(16) Vec<float, 4> {
     YAVL_VEC_ALIAS(float, 4)
@@ -97,12 +187,17 @@ struct alignas(16) Vec<float, 4> {
 
 #define MATH_RCP_EXPRS                                                  \
     {                                                                   \
-        
+        return Vec(rcp_sse42_impl(m));
     }
 
 #define MATH_SQRT_EXPRS                                                 \
     {                                                                   \
         return Vec(_mm_sqrt_ps(m));                                     \
+    }
+
+#define MATH_RSQRT_EXPRS                                                \
+    {                                                                   \
+        return Vec(rsqrt_sse52_impl(m));
     }
 
 #if defined(YAVL_X86_FMA)
