1cb6a8c38dfae0e4b67eb0c6560534ad1d6d7476
joeyskeys <joey.skeys@gmail.com>
2023-10-16 13:13:09 +0800

    add template tests

--- tests/vec_tests.cpp
+++ tests/vec_tests.cpp
@@ -68,30 +68,79 @@ TEST_CASE("Vector tests", "vector") {
     REQUIRE(v22.x == 2);
 }
 
-#define REQUIRE_ADAP(VEC, EXPR, VALUE)                                  \
+#define PROPER_EQUAL(EXPR, VALUE, VAR)                                  \
     {                                                                   \
-        if constexpr (std::is_floating_point_v(VEC::Scalar))            \
-            REQUIRE(VEC##EXPR == Approx(VALUE));                        \
+        if constexpr (std::is_floating_point_v<typename decltype(VAR)::Scalar>) \
+            REQUIRE(EXPR == Approx(VALUE));                             \
         else                                                            \
-            REQUIRE(VEC##EXPR == VALUE);                                \
+            REQUIRE(EXPR == VALUE);                                     \
     }
 
-TEMPLATE_PRODUCT_TEST_CASE("Vec tests", "[vec]", Vec, ((float, double, int, uint32_t), (2, 3, 4), (true, false))) {
+template <typename V>
+void check_vec_components(const V& v, const typename V::Scalar s) {
+    if constexpr (std::is_floating_point_v<typename V::Scalar>) {
+        REQUIRE(v.x == Approx(s));
+        REQUIRE(v.r == Approx(s));
+        REQUIRE(v.y == Approx(s));
+        REQUIRE(v.g == Approx(s));
+        if constexpr (V::Size > 2) {
+            REQUIRE(v.z == Approx(s));
+            REQUIRE(v.b == Approx(s));
+        }
+        if constexpr (V::Size > 3) {
+            REQUIRE(v.w == Approx(s));
+            REQUIRE(v.a == Approx(s));
+        }
+    }
+    else {
+        REQUIRE(v.x == s);
+        REQUIRE(v.r == s);
+        REQUIRE(v.y == s);
+        REQUIRE(v.g == s);
+        if constexpr (V::Size > 2) {
+            REQUIRE(v.z == s);
+            REQUIRE(v.b == s);
+        }
+        if constexpr (V::Size > 3) {
+            REQUIRE(v.w == s);
+            REQUIRE(v.a == s);
+        }
+    }
+}
+
+TEMPLATE_PRODUCT_TEST_CASE("Vec tests", "[vec]", (Vec2, Vec3, Vec4), (float, double, int, uint32_t)) {
+    // Ctor tests
     TestType vec0;
-    REQUIRE_ADAP(vec0, .x, 0);
-    REQUIRE_ADAP(vec0, .r, 0);
-
     TestType vec1{1};
-    REQUIRE_ADAP(vec1, .x, 1);
-    REQUIRE_ADAP(vec1, .r, 1);
-    REQUIRE_ADAP(vec1, .y, 1);
-    REQUIRE_ADAP(vec1, .g, 1);
-    if constexpr (vec1.Size > 2) {
-        REQUIRE_ADAP(vec1, .z, 1);
-        REQUIRE_ADAP(vec1, .b, 1);
+    TestType vec2{2};
+    TestType vec3{3};
+
+    SECTION("Ctor tests") {
+        check_vec_components(vec0, 0);
+        check_vec_components(vec1, 1);
     }
-    if constexpr (vec1.Size > 3) {
-        REQUIRE_ADAP(vec1, .w, 1);
-        REQUIRE_ADAP(vec1, .a, 1);
+
+    SECTION("Operator tests") {
+        auto vec4 = vec1 + vec2;
+        PROPER_EQUAL(vec4.x, 3, vec4);
+        PROPER_EQUAL(vec4.y, 3, vec4);
+
+        vec4 = vec2 - vec1;
+        PROPER_EQUAL(vec4.x, 1, vec4);
+        PROPER_EQUAL(vec4.y, 1, vec4);
+
+        vec4 = vec2 * vec3;
+        PROPER_EQUAL(vec4.x, 6, vec4);
+        PROPER_EQUAL(vec4.y, 6, vec4);
+
+        vec4 = vec3 / vec2;
+        if constexpr (std::is_floating_point_v<typename decltype(vec4)::Scalar>) {
+            REQUIRE(vec4.x == Approx(1.5));
+            REQUIRE(vec4.y == Approx(1.5));
+        }
+        else {
+            REQUIRE(vec4.x == 1);
+            REQUIRE(vec4.y == 1);
+        }
     }
 }
